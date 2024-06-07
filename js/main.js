f328ca40f82437bad708fbe27dd52d643e0ddbc8
Felipe <devfbraunstein@gmail.com>
2024-06-07 18:21:02 -0300

    Feito efeito de contador

--- js/main.js
+++ js/main.js
@@ -30,3 +30,54 @@ var swiper = new Swiper(".bg-slider-thumbs", {
   closeBtn.addEventListener("click", () => {
     navigation.classList.remove("active");
   });
+
+  //EFEITO CONTADOR
+  function medidas(){
+    const contador1 =document.getElementById("contador1");
+    let valor1=0;
+
+    //setInterval- ele define um derterminado intervalo de tempo
+
+    let tempo1 = setInterval(()=>{
+        valor1 +=1;
+        contador1.innerHTML = `+${valor1}`;
+
+        if(valor1 == 170){
+            //limpa o intervalo de tempo
+            clearInterval(tempo1);
+        }
+    })
+
+    //medidor 2
+    const contador2 =document.getElementById("contador2");
+    let valor2=0;
+
+    //setInterval- ele define um derterminado intervalo de tempo
+
+    let tempo2 = setInterval(()=>{
+        valor2 +=1;
+        contador2.innerHTML = `+${valor2}`;
+
+        if(valor2 == 23){
+            //limpa o intervalo de tempo
+            clearInterval(tempo2);
+        }
+    })
+
+    //medidor 3
+    const contador3 =document.getElementById("contador3");
+    let valor3=0;
+
+    //setInterval- ele define um derterminado intervalo de tempo
+
+    let tempo3 = setInterval(()=>{
+        valor3 +=1;
+        contador3.innerHTML = `+${valor3}`;
+
+        if(valor3 == 5){
+            //limpa o intervalo de tempo
+            clearInterval(tempo3);
+        }
+    })
+
+}
